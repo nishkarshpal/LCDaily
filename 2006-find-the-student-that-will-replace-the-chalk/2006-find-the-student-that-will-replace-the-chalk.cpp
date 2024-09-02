e96c8b7ab29e2345f1aaca33bd1639839282354c
Nishkarsh Pal <37383016+nishkarshpal@users.noreply.github.com>
2024-09-03 01:43:28 +0530

    Time: 94 ms (65.74%), Space: 84 MB (6.11%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
         int numStudents = chalk.size();
-        vector<long long> prefixSum(numStudents, chalk[0]);
+        vector<long long> prefixSum(numStudents, chalk[0]); //prefixsum
         for (int i = 1; i < numStudents; ++i) {
             prefixSum[i] = prefixSum[i - 1] + chalk[i];
         }
-        k = k % prefixSum[numStudents - 1];
+        k = k % prefixSum[numStudents - 1]; //modulo
         return std::upper_bound(prefixSum.begin(), prefixSum.end(), k) -
-               prefixSum.begin();
+               prefixSum.begin(); //binarysearch
     }
 };
